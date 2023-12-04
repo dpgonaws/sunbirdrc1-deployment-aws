a2958627c1f29115f14ebbfab9ec96c6eefa096e
Khan <mozkhan@5c523098b971.ant.amazon.com>
2023-12-04 17:54:35 +0530

    Consolidate code, update documentation, and finalize structure

--- lib/helm-stack.ts
+++ lib/helm-stack.ts
@@ -1,13 +1,13 @@
 import * as cdk from "aws-cdk-lib";
-import * as helm from "aws-cdk-lib/aws-eks";
-import { Construct } from "constructs";
-import * as eks from "aws-cdk-lib/aws-eks";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
-import { ConfigProps } from "./config";
-import { Secret } from "aws-cdk-lib/aws-secretsmanager";
-import * as s3 from "aws-cdk-lib/aws-s3";
+import * as eks from "aws-cdk-lib/aws-eks";
+import * as helm from "aws-cdk-lib/aws-eks";
 import * as iam from "aws-cdk-lib/aws-iam";
-import * as childProcess from 'child_process';
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as sm from "aws-cdk-lib/aws-secretsmanager";
+import { ISecret, Secret } from "aws-cdk-lib/aws-secretsmanager";
+import { Construct } from "constructs";
+import { ConfigProps } from "./config";
 
 export interface helmStackProps extends cdk.StackProps {
   config: ConfigProps;
@@ -15,6 +15,12 @@ export interface helmStackProps extends cdk.StackProps {
   rdssecret: string;
   eksCluster: eks.FargateCluster;
   s3bucket: s3.Bucket;
+  rdsHost: string;
+  KEYCLOAK_ADMIN_CLIENT_SECRET: string;
+  KEYCLOAK_ADMIN_PASSWORD: string;
+  KEYCLOAK_DEFAULT_USER_PASSWORD: string;
+  RDS_PASSWORD: string;
+  MINIO_USER: string;
 }
 
 export class helmStack extends cdk.Stack {
@@ -23,18 +29,27 @@ export class helmStack extends cdk.Stack {
     const vpc = props.vpc;
     const eksCluster = props.eksCluster;
     const rdssecretARN = props.rdssecret;
-    /* const appSecret = Secret.fromSecretCompleteArn(
-      this,
-      "rdssecret",
-      rdssecretARN
-    );
-    const user = appSecret.secretValueFromJson("username").toString();
-    const base64encodedpass = cdk.Fn.base64(
-      appSecret.secretValueFromJson("password").unsafeUnwrap.toString()
-    );
+    const bucketName = props.s3bucket.bucketName;
+    const KEYCLOAK_ADMIN_CLIENT_SECRET = props.KEYCLOAK_ADMIN_CLIENT_SECRET;
+    const KEYCLOAK_ADMIN_PASSWORD = props.KEYCLOAK_ADMIN_PASSWORD;
+    const KEYCLOAK_DEFAULT_USER_PASSWORD = props.KEYCLOAK_DEFAULT_USER_PASSWORD;
+    const RDS_PASSWORD = props.RDS_PASSWORD;
+    const MINIO_USER = props.MINIO_USER;
+
+    const secretName = sm.Secret.fromSecretAttributes(this, "ImportedSecret", {
+      secretCompleteArn: rdssecretARN,
+    });
+    const getValueFromSecret = (secret: ISecret, key: string): string => {
+      return secret.secretValueFromJson(key).unsafeUnwrap();
+    };
+    const dbPass = getValueFromSecret(secretName, "password");
+
+    const base64encodedDBpass = cdk.Fn.base64(RDS_PASSWORD);
+    const base64encodedkeycloakAdminPassword = cdk.Fn.base64(KEYCLOAK_ADMIN_PASSWORD);
+    const base64encodedkeycloakUserPassword = cdk.Fn.base64(KEYCLOAK_DEFAULT_USER_PASSWORD);
 
     const useriam = new iam.User(this, "MyUser", {
-      userName: "sbrc-minios", // Replace with your desired username
+      userName: MINIO_USER,
     });
     useriam.addManagedPolicy(
       iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess")
@@ -47,31 +62,23 @@ export class helmStack extends cdk.Stack {
     const encodedAccessKey = cdk.Fn.base64(
       accessKey.attrSecretAccessKey.toString()
     );
-    const encodedSecretKey = cdk.Fn.base64(accessKey.ref); */
-
-    // new helm.HelmChart(this, "SBRC-HelmChart", {
-    //   cluster: eksCluster,
-    //   chart: "azure-marketplace/nginx-test",
-    //   // ]chartAsset: ChartdAsset,
-    //   // release: "first-nginx",
-    //   values: {
-    //     "global.secrets.DB_PASSWORD": base64encodedpass,
-    //     "global.secrets.MINIO_SECRET_KEY": encodedSecretKey,
-    //     "global.secrets.access_key": encodedAccessKey,
-    //   },
-    //   repository: "https://marketplace.azurecr.io/helm/v1/repo", // Helm chart repository URL
-    // });
+    const encodedSecretKey = cdk.Fn.base64(accessKey.ref);
+    const chart = props.config.CHART;
+    const repository = props.config.REPOSITORY;
+    const namespace = props.config.NAMESPACE;
+    const release = props.config.RELEASE;
+    const rdsHost = props.rdsHost;
 
     new helm.HelmChart(this, "cdkhelm", {
       cluster: eksCluster,
-      chart: 'sunbird-rc',
-      repository: 'https://amitvashist7.github.io/helm-example/',
-      namespace: "sbrc-registry20",
-      //release: "sbrc-registry",
+      chart: chart,
+      repository: repository,
+      namespace: namespace,
+      release: release,
       values: {
         global: {
-          database: { 
-            host: "rdsstack-a-databaseb269d8bb-ooqkaknrwybr.cluster-cveklit2rj4m.ap-south-1.rds.amazonaws.com",
+          database: {
+            host: rdsHost,
           },
           kafka: {
             url: "kafka.sbrc.com:9098",
@@ -80,71 +87,27 @@ export class helmStack extends cdk.Stack {
             host: "redis.sbrc.com",
           },
           elastic_search: {
-            url:  "es.sbrc.com",
+            url: "es.sbrc.com",
           },
           secrets: {
-            DB_PASSWORD: "czBnQzg2TnY4S1Z3TFRSM0p6U28zLC15RWFoeXk3",
-            ELASTIC_SEARCH_PASSWORD: "T3BlbnNlYXJjaEAxMjMK",
-            KEYCLOAK_ADMIN_CLIENT_SECRET: "YzllOTA1YTQtOWIyZi00NWU2LThlMDUtMTNjM2E5NTNmNjUx",
-            KEYCLOAK_ADMIN_PASSWORD: "YWRtaW4xMjM=",
-            MINIO_SECRET_KEY: "a3pySHYxNldQSGQ4VWRUWVo0c1YxNjV3eFBvSlpGalV5SkVqVksxdg==", 
-            KEYCLOAK_DEFAULT_USER_PASSWORD: "YWRtaW5AMTIz",
-            access_key: "QUtJQVZNVk5DUVlDUEdNSERDTUE=",
+            DB_PASSWORD: base64encodedDBpass,
+            // ELASTIC_SEARCH_PASSWORD: ELASTIC_SEARCH_PASSWORD,
+            KEYCLOAK_ADMIN_CLIENT_SECRET: KEYCLOAK_ADMIN_CLIENT_SECRET,
+            KEYCLOAK_ADMIN_PASSWORD: base64encodedkeycloakAdminPassword,
+            KEYCLOAK_DEFAULT_USER_PASSWORD: base64encodedkeycloakUserPassword,
+            MINIO_SECRET_KEY: encodedSecretKey,
+            access_key: encodedAccessKey,
           },
           minio: {
-            bucket_key: "sbrc-registry-201", 
-            access_key: "QUtJQVZNVk5DUVlDUEdNSERDTUE=",
-          }
-        }  
-      }, 
-   });
+            bucket_key: bucketName,
+            access_key: encodedAccessKey,
+          },
+        },
+      },
+    });
 
-
-    /* eksCluster.addHelmChart('NginxIngress', {
-      chart: 'nginx-ingress',
-      repository: 'https://helm.nginx.com/stable',
-      namespace: 'sbrc-registry20',
-    }); */
-
-    // const vpc = ec2.Vpc.fromLookup(this, "SB-RCVPC", {
-    //   vpcId: "vpc-09c0c359d8d0537c7",
-    //   //   availabilityZones: ["aps1-az3", "aps1-az1"],
-    //   //   publicSubnetIds: ["subnet-0a906b8d151a34ea0", "subnet-0b88badfa7d4ac407"],
-    // });
-
-    // const cluster = eks.Cluster.fromClusterAttributes(this, "MyEksCluster", {
-    //   clusterName: "eks-sbrc-new-v2",
-    //   kubectlRoleArn: "arn:aws:iam::370803901956:user/vishwajeet",
-    //   vpc: vpc,
-    //   //   eks.Vpc.fromVpcAttributes(this, "MyVpc", {
-    //   //     vpcId: "vpc-09c0c359d8d0537c7",
-    //   //     availabilityZones: ["aps1-az3", "aps1-az1"],
-    //   //     publicSubnetIds: [
-    //   //       "subnet-0a906b8d151a34ea0",
-    //   //       "subnet-0b88badfa7d4ac407",
-    //   //     ],
-    //   //   }),
-    // });
-
-    // Define the local path to your Helm chart folder
-    // const helmChartFolder = "../helm-deployment/infra/helm_charts";
-
-    // Create a Helm chart using the local folder as the source
-    // new helm.HelmChart(this, "MyHelmChart", {
-    //   cluster: cluster,
-    //   chart: helmChartFolder,
-    //   release: "sunbirdrc",
-    //   values: {
-    //     "global.secrets.DB_PASSWORD":
-    //       "aSx6M3NEZ1B6dEV3Mm1eMllrPUVIXkZrbWUsX2Fe",
-    //     //"global.secrets.ELASTIC_SEARCH_PASSWORD":"abc",
-    //     //"global.secrets.KEYCLOAK_ADMIN_CLIENT_SECRET":"abc",
-    //     // "global.secrets.KEYCLOAK_ADMIN_PASSWORD":"password",
-    //     "global.secrets.MINIO_SECRET_KEY": "QUtJQVZNVk5DUVlDSTNIRTJCWEM=",
-    //     "global.secrets.access_key":
-    //       "QzJvenppM3ZRUlVsSndDb1RaWVRjSXBHY0VzSFQ2a00vTyt5MXozVw==",
-    //     //"global.minio.bucket_key":"abc",
-    //   },
-    // });
+    new cdk.CfnOutput(this, "DB Password", {
+      value: dbPass,
+    });
   }
 }
--- lib/rds-stack.ts
+++ lib/rds-stack.ts
@@ -1,46 +1,32 @@
 import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
-import * as rds from "aws-cdk-lib/aws-rds";
-import * as s3 from "aws-cdk-lib/aws-s3";
 import * as kms from "aws-cdk-lib/aws-kms";
-import { ConfigProps } from "./config";
-import { Stack, StackProps } from "aws-cdk-lib";
-import { vpcStack } from "./vpc-stack";
+import * as rds from "aws-cdk-lib/aws-rds";
 import { Secret } from "aws-cdk-lib/aws-secretsmanager";
-type AwsEnvStackProps = StackProps & {
-  config: Readonly<ConfigProps>;
-};
+import { Construct } from "constructs";
+import { ConfigProps } from "./config";
 
 export interface RdsStackProps extends cdk.StackProps {
   config: ConfigProps;
   vpc: ec2.Vpc;
+  rdsuser: String;
+  rdspassword: String;
 }
 
 export class rdsStack extends cdk.Stack {
   public readonly rdsSecret: string;
+  public readonly rdsHost: string;
   constructor(scope: Construct, id: string, props: RdsStackProps) {
     super(scope, id, props);
 
     const { config } = props;
     const RDS_SEC_GRP_INGRESS = config.RDS_SEC_GRP_INGRESS;
     const vpc = props.vpc;
+    const RDS_USER = props.rdsuser;
+    const RDS_PASSWORD = props.rdspassword;
     const kmsKey = new kms.Key(this, "RDSKmsKey", {
-      enableKeyRotation: true, // Optional: Enable key rotation
+      enableKeyRotation: true,
     });
-
-    // const vpcId = cdk.Fn.importValue("SB-RCVPC");
-    // console.log("VPCID-->  ", vpcId.toString());
-    // // const vpcId = ssm.StringParameter.valueFromLookup(
-    // //   this,
-    // //   "/VpcProvider/VPCID"
-    // // );
-    // const vpc = ec2.Vpc.fromLookup(this, "SB-RCVPC", {
-    //   vpcId: "vpc-09c0c359d8d0537c7",
-    // });
-    // const vpc = ec2.Vpc.fromLookup(this, "SB-RCVPC", {
-    //   vpcId: vpcId.toString(),
-    // });
 
     const securityGroupRDS = new ec2.SecurityGroup(this, "RdsSecurityGroup", {
       vpc: vpc,
@@ -48,26 +34,23 @@ export class rdsStack extends cdk.Stack {
       description: "Security group for RDS-Aurora Postgres",
     });
     securityGroupRDS.addIngressRule(
-      // ec2.Peer.ipv4("10.40.0.0/16"), //make configurable
       ec2.Peer.ipv4(RDS_SEC_GRP_INGRESS),
       ec2.Port.tcp(5432),
       "Allow RDS traffic"
     );
 
-    const subnetGroupRDS = new rds.SubnetGroup(this, "RDSSubnetGroup", {
-      description: "Subnet for RDS Aurora",
-      vpc: vpc,
-      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    const creds = new Secret(this, "rdsSecret", {
+      secretObjectValue: {
+        username: cdk.SecretValue.unsafePlainText(RDS_USER.toString()),
+        password: cdk.SecretValue.unsafePlainText(RDS_PASSWORD.toString()),
+      },
     });
-    // Create username and password secret for DB Cluster
-    const secret = new rds.DatabaseSecret(this, "AuroraSecret", {
-      username: "postgres",
-    });
+
     const cluster = new rds.DatabaseCluster(this, "Database", {
       engine: rds.DatabaseClusterEngine.auroraPostgres({
         version: rds.AuroraPostgresEngineVersion.VER_14_6,
       }),
-      credentials: rds.Credentials.fromSecret(secret),
+      credentials: rds.Credentials.fromSecret(creds),
       vpc: vpc,
       writer: rds.ClusterInstance.serverlessV2("writer"),
       serverlessV2MinCapacity: 2,
@@ -80,6 +63,7 @@ export class rdsStack extends cdk.Stack {
       },
     });
 
-    this.rdsSecret = secret.secretArn;
+    this.rdsSecret = creds.secretArn;
+    this.rdsHost = cluster.clusterEndpoint.hostname;
   }
 }
